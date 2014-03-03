04c5249777489dd980dc12d5b6fa29369dc043a5
Aurélien Thieriot <a.thieriot@gmail.com>
2014-03-03 18:59:37 +0000

    Add one solution into server

--- server.js
+++ server.js
@@ -71,6 +71,36 @@ app.post("/register", function(req, res) {
   });
 });
 
+app.get("/about", function(request, response){
+  response.json(200, {
+    name: 'Example FTW',
+    avatar: 'https://1.gravatar.com/avatar/0d40e29bf1ef6d51c9b8e6e7a0edd9f5?d=https%3A%2F%2Fidenticons.github.com%2F25b261712fe40e8558802d1a00745816.png&r=x&s=440',
+    city: 'Cardiff' 
+  });
+});
+
+var computeSolution = function(crime) {
+  switch (crime.type) {
+    case 'ROBBERY':
+      return {
+        _id: crime._id,
+        action: 'FINE',
+        amount: crime.amount * 2 + crime.amount * 0.2
+      };
+    default: return {};
+  }
+};
+
+app.post("/broadcast", function(request, response){
+  request.on('data', function(chunk) {
+    var crime = JSON.parse(chunk.toString());
+    var solution = computeSolution(crime);
+
+    console.log(JSON.stringify(crime) + "\n" + JSON.stringify(solution));
+    response.json(solution);
+  });
+});
+
 casual.define('crime_report', function() {
   return {
     criminal: {
